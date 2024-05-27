e0f0b89ad2dac4e2f7efa2b9cc81fea7c12771e8
mahibashardeveloper <mahibashar2023@gmail.com>
2024-05-28 01:46:28 +0600

    update vite js file

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/scss/style-sheet.scss',
-                'resources/js/user/app.js'
+                'resources/js/user/app.js',
+                'resources/js/admin/app.js',
             ],
             refresh: true,
         }),
