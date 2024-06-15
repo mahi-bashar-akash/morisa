68e627b6312a491e2a638397f14d0cf7f6804a93
mahibashardeveloper <mahibashar2023@gmail.com>
2024-06-15 10:47:12 +0600

    contact and profile ui on progress

--- resources/js/admin/pages/contact.vue
+++ resources/js/admin/pages/contact.vue
@@ -1,29 +1,6 @@
 <template>
 
-    <div class="card">
-        <div class="card-body">
-            <form class="row">
-                <div class="col-lg-4">
-                    <div class="form-group mb-3 mb-lg-0">
-                        <label for="phone-number" class="form-label fw-medium">Phone Number</label>
-                        <input id="phone-number" type="text" name="phone-number" class="form-control p-3 border rounded-0 shadow-none" required autocomplete="new-phone-number">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="form-group mb-3 mb-lg-0">
-                        <label for="email" class="form-label fw-medium">Email</label>
-                        <input id="email" type="email" name="email" class="form-control p-3 border rounded-0 shadow-none" required autocomplete="new-email">
-                    </div>
-                </div>
-                <div class="col-lg-4">
-                    <div class="form-group mb-3 mb-lg-0">
-                        <label for="address" class="form-label fw-medium">Address</label>
-                        <input id="address" type="text" name="address" class="form-control p-3 border rounded-0 shadow-none" required autocomplete="new-address">
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
+
 
 </template>
 
--- resources/js/admin/pages/profile.vue
+++ resources/js/admin/pages/profile.vue
@@ -1,5 +1,43 @@
 <template>
 
+    <div class="row justify-content-center align-items-center height-calc-90">
+        <div class="col-10 col-sm-7 col-md-6 col-lg-6 col-xl-4">
+            <div class="card bg-white border-0 rounded-0">
+                <div class="card-header bg-white border-0 rounded-0 d-flex justify-content-center pt-4 pb-3">
+                    <div class="width-150 height-150 rounded-circle shadow d-flex justify-content-center align-items-center">
+                        <i class="bi bi-plus text-theme fs-1"></i>
+                    </div>
+                </div>
+                <div class="card-body bg-white border-0 rounded-0 px-4 text-center">
+                    <div class="mb-3 fw-bold">
+                        Full Name
+                    </div>
+                    <div class="mb-3 text-secondary">
+                        Mahi Bashar Akash
+                    </div>
+                    <div class="mb-3 fw-bold">
+                        Phone Number
+                    </div>
+                    <div class="mb-3 text-secondary">
+                        +880 01400125289
+                    </div>
+                    <div class="mb-3 fw-bold">
+                        Email
+                    </div>
+                    <div class="mb-3 text-secondary">
+                        mahibashar2023@gmail.com
+                    </div>
+                    <div class="mb-3 fw-bold">
+                        Present Address
+                    </div>
+                    <div class="mb-3 text-secondary">
+                        Jessore Sadar, Jessore, Bangladesh
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
