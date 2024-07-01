88df81fb7200f240bda056c0b87277ec7a895e6c
mahibashardeveloper <mahibashar2023@gmail.com>
2024-07-01 09:31:49 +0600

    `update-file-name`

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\backend;
+namespace App\Http\Controllers\api;
 
 use App\Models\Admin;
 use App\Models\User;
--- routes/secure.php
+++ routes/secure.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\backend\AuthController;
+use App\Http\Controllers\api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
